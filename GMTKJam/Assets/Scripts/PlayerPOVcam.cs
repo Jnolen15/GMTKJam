fb4ef420cc11214da873b79d3b4bdec85794eb2c
Daebreak-II <70728190+Daebreak-II@users.noreply.github.com>
2023-07-08 10:01:58 -0700

    Updated playerPOVcam
    - changed playerpov cam to use coroutines instead of update
    - made it unable to observe the same object twice in a row
    - added random variance to the amount of time it views an object for

--- GMTKJam/Assets/Scripts/PlayerPOVcam.cs
+++ GMTKJam/Assets/Scripts/PlayerPOVcam.cs
@@ -7,8 +7,10 @@ public class PlayerPOVcam : MonoBehaviour
 
     public List<GameObject> Locations;
     public bool Observing = false;
-    public float ObserveTime;
-    public float LerpFloat; // 0 - 1
+    public GameObject CurrentlyObserving;
+    public float MinObserveTime;
+    public float MaxObserveTime;
+    public float LookSpeed; // 0 - 1
     private float TimeStamp;
     private int nombre;
 
@@ -23,25 +25,32 @@ public class PlayerPOVcam : MonoBehaviour
     {
         if(Observing)
         {
-            Observe(Locations[nombre], ObserveTime);
+            // Observe(Locations[nombre], ObserveTime);
         } else
         {
-            nombre = Random.Range(0, 3);
+            int previousNombre = nombre;
+            while(nombre == previousNombre) // prevents the same object from being observed twice
+            {
+                nombre = Random.Range(0, 3);
+            }
+            /*
             if (!Observing)
                 Observing = true;
                 TimeStamp = Time.time;
+            */
+            CurrentlyObserving = Locations[nombre];
             Debug.Log("Observing Nombre " + nombre);
+            StartCoroutine(TimedObserve(CurrentlyObserving, Random.Range(MinObserveTime, MaxObserveTime)));
         }
-        
     }
 
-    public void Observe(GameObject obj, float time)
+    public void Observe(GameObject obj, float time) // out of use, replaced by TimedObserve coroutine
     {
 
         // Debug.Log(TimeStamp + time);
 
 
-        this.transform.position = Vector3.Lerp(this.transform.position, obj.transform.position, LerpFloat);
+        this.transform.position = Vector3.Lerp(this.transform.position, obj.transform.position, 0.01f);
         if (Time.time >= TimeStamp + time)
             Observing = false;
             // Debug.Log(TimeStamp);
@@ -51,5 +60,33 @@ public class PlayerPOVcam : MonoBehaviour
     {
 
     }
+
+    IEnumerator TimedObserve(GameObject obj, float ObserveTime)
+    {
+        // two parts, first lerps to obj, then a second, faster lerp to stick it to that obj
+        Observing = true;
+        float time = 0;
+
+        while(time < LookSpeed)
+        {
+            float t = time / LookSpeed;
+            t = t * t * (3f - 2f * t);
+            this.transform.position = Vector3.Lerp(this.transform.position, obj.transform.position, t);
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        while (time < ObserveTime)
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, obj.transform.position, 1);
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        Observing = false;
+    }
+        
 
 }
